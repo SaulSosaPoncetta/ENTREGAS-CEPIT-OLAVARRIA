31645c4745a3c254d27afed4b2da8f0d288fcb34
SaulSosaPoncetta <shsp1975servicios@gmail.com>
2024-05-25 23:31:49 -0300

    se actualizan actividades

---
+++ 02paroimpar.ts
@@ -0,0 +1,8 @@
+/* Realice un programa que devuelva la potencia de un número.
+La base y el exponente deben ser ingresados por teclado.
+Sólo deben admitirse exponentes mayores o iguales a cero.
+Recuerde que el resultado de un número elevado a 0 es 1.
+Separe la lógica de calcular la potencia utilizando métodos.
+(se resuelve con funciones) */
+
+

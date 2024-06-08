e8cce7cf59fa30aea45a6daa020854116f48e1e0
SaulSosaPoncetta <165228294+SaulSosaPoncetta@users.noreply.github.com>
2024-06-08 19:18:09 -0300

    Se reorganizan trabajos obligatorios y practicas en general

--- 02paroimpar.ts
+++
@@ -1,8 +0,0 @@
-/* Realice un programa que devuelva la potencia de un número.
-La base y el exponente deben ser ingresados por teclado.
-Sólo deben admitirse exponentes mayores o iguales a cero.
-Recuerde que el resultado de un número elevado a 0 es 1.
-Separe la lógica de calcular la potencia utilizando métodos.
-(se resuelve con funciones) */
-
-

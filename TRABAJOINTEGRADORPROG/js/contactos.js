d7ff528bb9c955f8bac8e6d61d94ef2d407f5cda
shsp1975 <shsp1975servicios@gmail.com>
2024-08-05 20:37:03 -0300

    Se actualizan estructuras

--- TRABAJOINTEGRADORPROG/js/contactos.js
+++ TRABAJOINTEGRADORPROG/js/contactos.js
@@ -11,10 +11,10 @@ btnform.addEventListener("click", (e) => {
     // Verifica si alguno de los campos está vacío
     if (!nombre.value || !apellido.value || !telefono.value || !email.value || !mensaje.value) {
         alert("Por favor, completa todos los campos.");
-        return; // Detiene la ejecución del resto del código
+        return; 
     }
 
-    // Si todos los campos están completos, guarda la información
+    
     let informacion = [];
     informacion[0] = nombre.value;
     informacion[1] = apellido.value;

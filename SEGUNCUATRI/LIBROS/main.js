365f34dfdaecc894893bd7da3f0d5b0e5f746049
Saul <shsp1975servicios@gmail.com>
2024-10-08 02:30:20 +0200

    actualizo entregables

--- SEGUNCUATRI/LIBROS/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gestorlibros_1 = require("./gestorlibros");
-//let Aventuras: libro = new libro("Cinco Semanas En Globo", "Julio Verne", 1942);
-var Biblioteca1 = new gestorlibros_1.Gestorlibro("Biblioteca Collinet");
-Biblioteca1.agregarLibro("5 semanas globo", "julio verne", 1942);
-console.log(Biblioteca1.getListaLibros());
